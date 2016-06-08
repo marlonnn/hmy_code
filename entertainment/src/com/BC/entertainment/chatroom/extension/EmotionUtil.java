9d31996ec9bc9b0a17f4ca67f5060fc5808c60a4
marlonnn <zhongw_1988@126.com>
2016-06-08 10:26:59 +0800

    judge user can send custom message before send

--- entertainment/src/com/BC/entertainment/chatroom/extension/EmotionUtil.java
+++ entertainment/src/com/BC/entertainment/chatroom/extension/EmotionUtil.java
@@ -1,6 +1,7 @@
 package com.BC.entertainment.chatroom.extension;
 
 import com.BC.entertainment.cache.InfoCache;
+import com.netease.nimlib.sdk.msg.constant.MsgTypeEnum;
 import com.netease.nimlib.sdk.msg.model.IMMessage;
 
 public class EmotionUtil {
@@ -18,42 +19,51 @@ public class EmotionUtil {
     	{
     		try {
 				CustomAttachment customAttachment = (CustomAttachment)message.getAttachment();
-				switch(customAttachment.getType())
+				
+				if (message.getMsgType() == MsgTypeEnum.text)
 				{
-				/**
-				 * 表情
-				 */
-				case CustomAttachmentType.emotion:
-					EmotionAttachment emotionAttachment = (EmotionAttachment)customAttachment;
-					if (emotionAttachment != null)
-					{
-						if (emotionAttachment.getEmotion().getValue() < Integer.parseInt(InfoCache.getInstance().getPersonalInfo().getEntertainment_dollar()))
-						{
-							can = true;
-						}
-					}
-					break;
-				/**
-				 * 字体
-				 */
-				case CustomAttachmentType.font:
-					FontAttachment fontAttachment = (FontAttachment)customAttachment;
-					if (fontAttachment != null)
-					{
-						if (fontAttachment.getEmotion().getValue() < Integer.parseInt(InfoCache.getInstance().getPersonalInfo().getEntertainment_dollar()))
-						{
-							can = true;
-						}
-					}
-					break;
-				/**
-				 * 气泡
-				 */
-				case CustomAttachmentType.bubble:
-					//气泡暂时不需要娱币
 					can = true;
-					break;
 				}
+				else if (message.getMsgType() == MsgTypeEnum.custom)
+				{
+					switch(customAttachment.getType())
+					{
+					/**
+					 * 表情
+					 */
+					case CustomAttachmentType.emotion:
+						EmotionAttachment emotionAttachment = (EmotionAttachment)customAttachment;
+						if (emotionAttachment != null)
+						{
+							if (emotionAttachment.getEmotion().getValue() < Integer.parseInt(InfoCache.getInstance().getPersonalInfo().getEntertainment_dollar()))
+							{
+								can = true;
+							}
+						}
+						break;
+					/**
+					 * 字体
+					 */
+					case CustomAttachmentType.font:
+						FontAttachment fontAttachment = (FontAttachment)customAttachment;
+						if (fontAttachment != null)
+						{
+							if (fontAttachment.getEmotion().getValue() < Integer.parseInt(InfoCache.getInstance().getPersonalInfo().getEntertainment_dollar()))
+							{
+								can = true;
+							}
+						}
+						break;
+					/**
+					 * 气泡
+					 */
+					case CustomAttachmentType.bubble:
+						//气泡暂时不需要娱币
+						can = true;
+						break;
+					}
+				}
+
 			} catch (Exception e) {
 				can = false;
 				e.printStackTrace();
