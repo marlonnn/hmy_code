6dae7dc6a45734cae95984bbba3b4da2dd6f88e0
marlonnn <zhongw_1988@126.com>
2016-06-20 13:56:12 +0800

    add region dialog

--- entertainment/src/com/BC/entertainmentgravitation/BaseInfoActivity.java
+++ entertainment/src/com/BC/entertainmentgravitation/BaseInfoActivity.java
@@ -25,7 +25,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
